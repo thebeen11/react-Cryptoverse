3b80b0ee91c8a6955e64d60afce5a05cf0a67024
thebeen11 <bagus11nugroho@gmail.com>
2022-04-14 00:18:39 +0700

    create store and cryptodetails

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,21 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import millify from "millify";
 import { Link } from "react-router-dom";
 import { Card, Row, Col, Input } from "antd";
 
 import { useGetCryptosQuery } from "../services/cryptoApi";
 
-const Cryptocurrencies = ({simplified}) => {
+const Cryptocurrencies = ({ simplified }) => {
   const count = simplified ? 10 : 100;
   const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
-  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+  const [cryptos, setCryptos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-  if(isFetching) return 'Loading...';
+  useEffect(() => {
+    const filterData = cryptosList?.data?.coins.filter((coin) =>
+      coin.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setCryptos(filterData);
+  }, [cryptosList, searchTerm]);
+
+
+  if (isFetching) return "Loading...";
 
   return (
     <>
+      {!simplified &&(
+          <div className="search-crypto">
+          <Input
+            placeholder="Search Cryptocurrency"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      )}
       <Row gutter={[32, 32]} className="crypto-card-container">
-        {cryptos.map((currency) => (
+        {cryptos?.map((currency) => (
           <Col
             xs={24}
             sm={12}
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,23 +1,25 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoApiHeaders = {
-    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
-    'X-RapidAPI-Key': '0558395c17mshac1b01f1f41ef58p1f8084jsnb9e84bc0ea7f'
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+  "X-RapidAPI-Key": "0558395c17mshac1b01f1f41ef58p1f8084jsnb9e84bc0ea7f",
 };
 
 const baseUrl = "https://coinranking1.p.rapidapi.com";
 
-const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
 export const cryptoApi = createApi({
-    reducerPath: 'cryptoApi',
-    baseQuery: fetchBaseQuery({baseUrl}),
-    endpoints: (builder) =>({
-        getCryptos: builder.query({
-            query: (count) => createRequest(`/coins?limit=${count}`)
-        })
-    })
-})
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+    getCryptosDetails: builder.query({
+      query: (coinID) => createRequest(`/coin/${coinID}`),
+    }),
+  }),
+});
 
-
-export const {useGetCryptosQuery} = cryptoApi
+export const { useGetCryptosQuery, useGetCryptosDetailsQuery } = cryptoApi;
